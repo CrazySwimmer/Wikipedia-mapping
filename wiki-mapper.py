f67512a28573213a9ce92da69c453c87eee5c227
patrikwalchli <34949095+patrikwalchli@users.noreply.github.com>
2018-01-02 20:44:24 +0100

    update patrik
    solving the breadth problem with depth first code

--- wiki-mapper.py
+++ wiki-mapper.py
@@ -4,6 +4,7 @@
 #Load modules
 from bs4 import BeautifulSoup
 import requests
+import numpy as np
     
 #==============================================================================
 #Get links from article
@@ -30,12 +31,22 @@ def get_internal_links(article):
 
 #Variables
 article = 'Metallica' #Starting article
-links = get_internal_links(article)
-maxItems=1000 #Set a limit to the crawling
-maxBreadth=1
+searchFirst='depth' #'breadth' or 'depth' search
+
+#Set a limit to the crawling
+maxItems=10
+maxBreadth=0
+maxDepth=0
+skip = False #Boolean used for depth-search only
+
 itemCount = 0 #Initialize the item counter
+links = get_internal_links(article)
 network = {article:links} #Initialize the output dictionary
+depth = {article: 1} #Initialize the depth dictionary, for maxDepth control
 scraped = [] #List of fully scraped articles (all internal links have been scraped according to maxBreadth/Depth)
+depth_scraped = [] #Similar list as above for depth-search only
+depthSearch = (searchFirst=='depth') #Set the boolean to handle depth-first search
+
 
 print('Variables declared: OK') #Temporary debug line
 
@@ -46,32 +57,55 @@ if maxBreadth > 0:
         
 #Repeat this step until we reach the maxItems threshold
 while itemCount <= maxItems:
-    #Iterate over the keys who aren't fully scraped according to maxBreadth
-    keys = [k for k in network.keys() if k not in scraped]
+    #Iterate over the keys who aren't fully scraped according to maxBreadth/Depth (nodes)
+    if depthSearch:
+        keys = [k for k in network.keys() if k not in depth_scraped]
+        if len(keys)==0: #Max depth reached
+            keys = [k for k in network.keys() if k not in scraped]
+    else:
+        keys = [k for k in network.keys() if k not in scraped]
+    
     #We have scraped everything allowed by maxBreadth/Depth
     if len(keys)==0:
         break
+    
     #Iterate over nodes that haven't been scraped
     for k in keys:
-        print('key :', k) #Temp debug line
-        #Iterate over the values (edges / relationships / internal links)
-        for v in get_internal_links(k):
-            print(k, '-', 'Item', itemCount, ':', v) #Temp debug line
-            if v not in network.keys(): #Add them to the keys if not present
-                links = get_internal_links(v) #Get the internal links
-                if maxBreadth == 0: #Set the max breadth as large as possible
-                    maxBreadth = len(links)
-                    mB = maxBreadth
-                else:
-                    mB = maxBreadth
+        if maxDepth == 0 or maxDepth > depth[k]:
+            print('key :', k) #Temp debug line
+            #Iterate over the values (edges / relationships / internal links)
+            for v in get_internal_links(k):
+                print(k, '-', 'Item', itemCount, ':', v) #Temp debug line
+                if v not in network.keys(): #Add them to the keys if not present
+                    links = get_internal_links(v) #Get the internal links
+                    if maxBreadth == 0: #Set the max breadth as large as possible
+                        maxBreadth = len(links)
+                        mB = maxBreadth
+                    #Special case for depth-first search
+                    elif depthSearch:
+                        mB = maxBreadth #Use np.isin() with numpy 1.13
+                        while len(links[:mB]) - sum(np.in1d(links[:mB], list(network.keys()))) == 0:
+                            mB += 1
+                    else:
+                        mB = maxBreadth
+                    
                 #Add the edge entry as a node 
                 network[v] = links[:mB] #Only assign links up to maxBreadth
+                depth[v] = depth[k] + 1 #Set the depth on the children-node
                 itemCount += 1
 
                 #Exit if we have reached the max item threshold
                 if itemCount >= maxItems:
                     break
-
-        scraped.append(k) #The node doesn't have to be scraped again
+                if depthSearch:
+                    depth_scraped.append(k) #Append the father-node to the special list
+                    skip = True
+                    break #Only add the first edge as a node
+        if skip==True:
+            skip = False
+            break
+        else:
+            scraped.append(k) #The node doesn't have to be scraped again
+            depth_scraped.append(k)
         if itemCount >= maxItems:
             break
